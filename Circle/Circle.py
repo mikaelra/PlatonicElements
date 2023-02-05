29f175810aea42ef19dbcb40ab9161d85ccef75e
Mikael Ravndal <mikael.ravndal@if.no>
2023-02-05 23:21:40 +0100

    Decouple a bit

--- Circle/Circle.py
+++ Circle/Circle.py
@@ -3,6 +3,9 @@ from matplotlib.pyplot import *
 
 
 def Circle(x0, y0, r, n):
+    # x0 and y0 are center coordinates of circle
+    # r is radius
+    # n is the amount of points plotted
     x = []
     y = []
 
@@ -15,22 +18,3 @@ def Circle(x0, y0, r, n):
     
     
     return [x, y]
-
-c0 = Circle(0,0,1,100)
-c1 = Circle(0,1,1,100)
-c2 = Circle(0,-1,1,100)
-c3 = Circle(math.sqrt(3)/2, 1/2,1,100)
-c4 = Circle(-math.sqrt(3)/2, 1/2,1,100)
-c5 = Circle(math.sqrt(3)/2, -1/2,1,100)
-c6 = Circle(-math.sqrt(3)/2, -1/2,1,100)
-
-
-plot(c0[0],c0[1])
-plot(c1[0],c1[1])
-plot(c2[0],c2[1])
-plot(c3[0],c3[1])
-plot(c4[0],c3[1])
-plot(c5[0],c5[1])
-plot(c6[0],c6[1])
-
-show()
---
+++ Circle/SeedOfLife.py
@@ -0,0 +1,22 @@
+from matplotlib.pyplot import *
+from Circle import Circle
+import math
+
+c0 = Circle(0,0,1,100)
+c1 = Circle(0,1,1,100)
+c2 = Circle(0,-1,1,100)
+c3 = Circle(math.sqrt(3)/2, 1/2,1,100)
+c4 = Circle(-math.sqrt(3)/2, 1/2,1,100)
+c5 = Circle(math.sqrt(3)/2, -1/2,1,100)
+c6 = Circle(-math.sqrt(3)/2, -1/2,1,100)
+
+
+plot(c0[0],c0[1])
+plot(c1[0],c1[1])
+plot(c2[0],c2[1])
+plot(c3[0],c3[1])
+plot(c4[0],c3[1])
+plot(c5[0],c5[1])
+plot(c6[0],c6[1])
+
+show()
